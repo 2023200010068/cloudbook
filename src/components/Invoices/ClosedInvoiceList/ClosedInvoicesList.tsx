e9c0f0cfb0b6230f8cefe23c240d319b727dd8d9
2023200010078 <2023200010078@seu.edu.bd>
2025-12-20 18:13:36 +0600

    10078: close invoice list

---
+++ src/components/Invoices/ClosedInvoiceList/ClosedInvoicesList.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { Breadcrumb } from "./Breadcrumb";
+import { InvoiceApiResponse, InvoiceData } from "@/src/types/invoices";
+import { useAuth } from "@/src/contexts/AuthContext";
+import { ClosedInvoicesListTable } from "./ClosedInvoicesListTable";
+import { useAccUserRedirect } from "@/src/hooks/useAccUser";
+
+export const ClosedInvoicesListComponent = () => {
+  const { user } = useAuth();
+  const [invoicesData, setInvoicesData] = useState<InvoiceData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useAccUserRedirect();
+
+  const fetchInvoices = useCallback(async () => {
+    if (!user?.id) return;
+    setLoading(true);
+
+    try {
+      const response = await fetch(`/api/invoices?user_id=${user.id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const json: InvoiceApiResponse = await response.json();
+
+      if (!response.ok || !json.success) {
+        throw new Error(json.message || "Failed to fetch invoices");
+      }
+
+      setInvoicesData(json.data);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [user?.id]);
+
+  useEffect(() => {
+    fetchInvoices();
+  }, [fetchInvoices]);
+
+  return (
+    <main className="bg-[#F2F4F7] min-h-screen p-5">
+      <Breadcrumb />
+      <ClosedInvoicesListTable
+        invoices={invoicesData}
+        fetchInvoices={fetchInvoices}
+        loading={loading}
+      />
+    </main>
+  );
+};
